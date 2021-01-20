214eaf8e3df6327096d6bfb4913ca7293fa8334c
erickcharlesneves <erick.neves.projetos@gmail.com>
2021-01-20 10:26:57 -0300

    Estilização do LayoutHeader corrigida

--- src/components/LayoutHeader.js
+++ src/components/LayoutHeader.js
@@ -5,108 +5,94 @@ import LogoImg from "../imgs/elo4-logo-cortada.png"
 
 const BoxHeader = styled.div`
   
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  
-  
-    nav {
-        width: 100%;
-        background-color: #F2970B;
-        height: 80px;
-        display: flex;
-        justify-content: space-between;
-        padding: 15px;
-        align-items: center;
-        z-index: 1;
-        /* position: fixed; */
-        box-shadow: 0px 5px 10px -3px;
-    }
-    a {
-      display: flex;
-      justify-content: center;
-      width: 150px;
-      cursor: pointer;
-    }
-    .logo-box {
-      display: flex;
-      justify-content: center;
-      width: 150px;
-      }
-    img {
-        display: flex;
-        justify-content: center;
-        align-items: center;   
-    }
-
-  
-    ul {
-      list-style: none;
-      display: flex;
-      font-family: "Roboto", sans-serif;
-    }
-    li {
-      margin: 5px 50px;
-      
-    }
-    button {
-      padding: 5px;
-      height: 40px;
-      width: 150px;
-      background: #F2F2F2;
-      border-radius: 20px;
-      border-width: 2px;
-    
-      cursor: pointer;
-    }
-    /* .quemSomos-link {
-      display: flex;
-      justify-content: center;
-      padding: 5px;
-      height: 40px;
-      width: 150px;
-      text-decoration: 1px solid;
-      } */
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 15vh;
     `
+
+const NavBar = styled.nav`
+  
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 15vh;
+    width: 100vw;
+    background-color: #F2970B;
+    `
+const Hiperlinks = styled.a`
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+`
+const ImgLogoHeader = styled.img`
+  
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 15vh; 
+`
+const UlistasBtnHeader = styled.ul`
+  
+    list-style: none;
+    display: flex;
+    align-items: center;
+    font-family: "Roboto", sans-serif;
+`
+const ListaBtnHeader = styled.li`
+  
+    margin: 0vh 10vh;
+    font-size: 3.2vh;
+`
+const BotaoHeader = styled.button`
+  
+    padding: 1vh;
+    height: 6vh;
+    width: 25vh;
+    border-radius: 20vh;
+    border-width: 0.2vh;
+    background: #F2F2F2;
+    cursor: pointer;
+    font-size: 2.5vh;
+`
 
 
 export class LayoutHeader extends React.Component {
     render() {
         return (
-            <div>
                 <BoxHeader>
-                    <nav>
-                        <div className="logo-box">
-                            <a onClick={this.irParaHome}>
-                                <img src={LogoImg} alt="logo elo4" />
-                            </a>
+                    <NavBar>
+                        <div>
+                            <Hiperlinks onClick={this.irParaHome}>
+                                <ImgLogoHeader src={LogoImg} alt="logo elo4" />
+                            </Hiperlinks>
                         </div>
-                        <ul>
-                            <li>
-                                <a className="quemSomos-link" onClick={this}>
+                        <UlistasBtnHeader>
+                            <ListaBtnHeader>
+                                <Hiperlinks onClick={this}>
                                     Quem somos?
-                                </a>
-                            </li>
-                            <li>
-                                <button onClick={this}>
+                                </Hiperlinks>
+                            </ListaBtnHeader>
+                            <ListaBtnHeader>
+                                <BotaoHeader onClick={this}>
                                     Produtos
-                                </button>
-                            </li>
-                            <li>
-                                <button onClick={this}>
+                                </BotaoHeader>
+                            </ListaBtnHeader>
+                            <ListaBtnHeader>
+                                <BotaoHeader onClick={this}>
                                     Anunciar Produtos
-                                </button>
-                            </li>
-                            <li>
-                                <button onClick={this}>
+                                </BotaoHeader>
+                            </ListaBtnHeader>
+                            <ListaBtnHeader>
+                                <BotaoHeader onClick={this}>
                                     Carrinho
-                                </button>
-                            </li>
-                        </ul>
-                    </nav>
+                                </BotaoHeader>
+                            </ListaBtnHeader>
+                        </UlistasBtnHeader>
+                    </NavBar>
                 </BoxHeader>
-            </div>
         )
     }
 }
