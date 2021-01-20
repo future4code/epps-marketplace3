b1b0f6a0727ed4ae3067c99b21a86fbc9905879a
erickcharlesneves <erick.neves.projetos@gmail.com>
2021-01-20 13:43:28 -0300

    Alterado caminho do header no AppContainer.js

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Footer from "../components/Footer/Footer";
 import Home from "../components/Home/Home"
-import { LayoutHeader } from "./LayoutHeader";
+import { LayoutHeader } from "../components/Header/LayoutHeader";
 
 
 export class AppContainer extends Component {
